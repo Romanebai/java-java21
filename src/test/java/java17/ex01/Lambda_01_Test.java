bfa8cc1b818ca6b6c6690932260d5478954077bb
Rohmane <R.S.T.E@hotmail.fr>
2025-12-09 14:39:20 +0100

    Exo 1 & 2 - expressions lambdas

--- src/test/java/java17/ex01/Lambda_01_Test.java
+++ src/test/java/java17/ex01/Lambda_01_Test.java
@@ -1,5 +1,6 @@
 package java17.ex01;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
 import java17.data.Data;
@@ -57,7 +58,9 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
+        PersonPredicate predicate = (Person p) -> p.getFirstname().equals("first_10");
+        List<Person> result = filter(personList, predicate);
+
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("first_10");
@@ -75,7 +78,12 @@ public class Lambda_01_Test {
 
         // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
         // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
+        PersonPredicate predicate = (Person p) -> {
+                return DigestUtils.sha512Hex(p.getPassword()).equals(passwordSha512Hex)&& (p.getAge()>49);
+        };
+        List<Person> result = filter(personList, predicate);
+
+
 
         assert result.size() == 6;
         for (Person person : result) {
