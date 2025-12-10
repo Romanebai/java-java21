51da44497adf50f2d4cdd8f753951f70d9cb6abd
Rohmane <R.S.T.E@hotmail.fr>
2025-12-10 15:07:16 +0100

    Exo de 1 à 4

--- src/test/java/java17/ex04/Function_04_Test.java
+++ src/test/java/java17/ex04/Function_04_Test.java
@@ -3,6 +3,7 @@ package java17.ex04;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -29,18 +30,14 @@ public class Function_04_Test {
     // PART 1 - ADULT
 
     // tag::adult[]
-    // TODO Compléter la fonction
-    // TODO AGE >=18
-    Predicate<Person> adult = null;
+    Predicate<Person> adult = person -> person.getAge()>=18;
     // end::adult[]
 
     @Test
     public void test_predicate() throws Exception {
 
         List<Person> personList = Data.buildPersonList();
-
-        // TODO invoquer la méthode filter pour que le test soit passant
-        List<Person> result = null;
+        List<Person> result = filter(personList,adult);
 
         assert result.size() == 4;
 
@@ -49,14 +46,12 @@ public class Function_04_Test {
     // PART 2 - ADULT AND LASTNAME=France AND FIRSTNAME=Armor
 
     // tag::predicateand[]
-    // TODO compléter la fonction
-    // TODO le prédicat vérifie que le nom est "France"
-    Predicate<Person> lastnameIsFrance = null;
+    // Le prédicat vérifie que le nom est "France"
+    Predicate<Person> lastnameIsFrance = person -> person.getLastname().equals("France");
 
 
-    // TODO compléter la fonction
-    // TODO le prédicat vérifie que le prénom est "Armor"
-    Predicate<Person> firstnameIsArmor = null;
+    //Le prédicat vérifie que le prénom est "Armor"
+    Predicate<Person> firstnameIsArmor = p -> p.getFirstname().equals("Armor");
     // end::predicateand[]
 
     @Test
@@ -64,9 +59,8 @@ public class Function_04_Test {
 
         List<Person> personList = Data.buildPersonList();
 
-        // TODO invoquer la méthode filter pour que le test soit passant
         // TODO chaîner les prédicats adult, lastnameIsFrance et firstnameIsArmor avec la méthode and
-        List<Person> result = null;
+        List<Person> result = filter(personList,adult.and(firstnameIsArmor).and(lastnameIsFrance));
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("Armor");
